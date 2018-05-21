ad146e1cafbad98394d2e2e305e294d38f7b2242
Merlin Thomas <emteeware@gmail.com>
2018-05-21 17:15:15 +0200

    Introduce some main flow

--- src/main/kotlin/net/emteeware/EmteeShovel.kt
+++ src/main/kotlin/net/emteeware/EmteeShovel.kt
@@ -1,6 +1,11 @@
 package net.emteeware
 
+import java.net.URI
+
 fun main(args: Array<String>) {
     println("Shoveling data …")
+    val mediaImporter = MediaImporter()
+    val importMediaList = mediaImporter.importMediaList(URI(args[0]))
+    importMediaList.sorted().forEach {m ->println(m)}
 }
 
