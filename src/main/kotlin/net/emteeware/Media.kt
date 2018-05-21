921bc42ae16d4c089246456b6e02f8cfb8641a73
Merlin Thomas <dev@emteeware.net>
2018-05-21 13:36:49 +0200

    Make Media comparable

--- src/main/kotlin/net/emteeware/Media.kt
+++ src/main/kotlin/net/emteeware/Media.kt
@@ -2,4 +2,12 @@ package net.emteeware
 
 import java.time.LocalDateTime
 
-data class Media(val imdbId: String, val name : String, val viewDate : LocalDateTime, val type : TraktMediaType, val rating : Int, val ratingDate : LocalDateTime)
+data class Media(val imdbId: String, val name : String, val viewDate : LocalDateTime, val type : TraktMediaType, val rating : Int, val ratingDate : LocalDateTime) : Comparable<Media> {
+    override fun compareTo(other: Media): Int {
+        if(imdbId == other.imdbId) {
+            return viewDate.compareTo(other.viewDate)
+        } else {
+            return imdbId.compareTo(other.imdbId)
+        }
+    }
+}
