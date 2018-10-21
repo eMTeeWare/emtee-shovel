c1735014d860573bea020904d3211404fbd6c495
Merlin Thomas <emteeware@gmail.com>
2018-10-21 14:41:23 +0200

    Remove more dead code
    Now, nearly nothing is left :(

--- src/main/kotlin/net/emteeware/EmteeShovel.kt
+++ src/main/kotlin/net/emteeware/EmteeShovel.kt
@@ -1,7 +1,5 @@
 package net.emteeware
 
-import com.natpryce.konfig.*
-import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
 import java.time.LocalDate
 import java.time.Period
 
@@ -18,16 +16,5 @@ fun main(args: Array<String>) {
     imdbViewingHistory.removeUndefined() // Some of these should be handled as episodes or movies, there should be a manual process for that
     imdbViewingHistory.removeDuplicateCheckInsWithin(Period.ofDays(14))
     imdbViewingHistory.print()
-    System.exit(0)
-    val userName = Key("user.name", stringType)
-    val config =
-            systemProperties() overriding
-            EnvironmentVariables() overriding
-            // ConfigurationProperties.fromFile() overriding
-            ConfigurationProperties.fromResource("defaults.properties")
-    println("Hallo ${config[userName]}")
-
-    val mediaImporter = MediaImporter()
-    mediaImporter.executeImport(args)
 }
 
--- src/main/kotlin/net/emteeware/TvShow.kt
+++
@@ -1,8 +0,0 @@
-package net.emteeware
-
-import java.time.LocalTime
-
-data class TvShow(
-        val name : String,
-        var defaultWatchTime: LocalTime = LocalTime.MIDNIGHT
-)
