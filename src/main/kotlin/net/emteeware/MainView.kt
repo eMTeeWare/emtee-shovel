eba9f719defc718b6cb911c6527badbf6550b3ed
Merlin Thomas <emteeware@gmail.com>
2019-01-02 14:10:31 +0100

    Introduce dedicated class for media to be shown / uploaded

--- src/main/kotlin/net/emteeware/MainView.kt
+++ src/main/kotlin/net/emteeware/MainView.kt
@@ -4,7 +4,7 @@ import javafx.collections.FXCollections
 import javafx.scene.Node
 import tornadofx.*
 import java.net.URL
-import java.util.*
+import java.time.LocalDate
 
 class MainView : View("My View") {
     override val root = borderpane {
@@ -14,13 +14,14 @@ class MainView : View("My View") {
     private fun centerVBox(): Node {
         return vbox {
             val media = FXCollections.observableArrayList(
-                    ImportedMedia(Position=2542, Const="kk4934924", Created=Date(118,3,18), Modified=Date(118,3,18), Description="", Title="Ydakr Eadsjo", URL= URL("https://www.example.com/title/kk4934924/"), TitleType= TraktMediaType.EPISODE, IMDbRating="8,2", Runtime=45, Year=2017, Genres="Drama", NumVotes=1738, ReleaseDate=Date(116,5,30), Directors="Lago Majore", YourRating=7, DateRated=Date(118,3,19)),
-                    ImportedMedia(Position=2542, Const="kk4934923", Created=Date(118,7,24), Modified=Date(118,7,24), Description="", Title="OIHlkasd Aasec", URL= URL("https://www.example.com/title/kk4934923/"), TitleType= TraktMediaType.EPISODE, IMDbRating="8,2", Runtime=45, Year=2017, Genres="Drama", NumVotes=1738, ReleaseDate=Date(114,5,30), Directors="Baik O'Nur", YourRating=7, DateRated=Date(118,3,19))
+                    Media(1, "kk2342", LocalDate.of(2018, 4, 21), "asdfas", URL("https://www.example.com/kk2342"), TraktMediaType.EPISODE, 8),
+                    Media(8, "kk2382", LocalDate.of(2018, 2, 21), "sda5dhf", URL("https://www.example.com/kk2382"), TraktMediaType.MOVIE, 3),
+                    Media(9, "kk2392", LocalDate.of(2018, 6, 21), "asd45", URL("https://www.example.com/kk2392"), TraktMediaType.EPISODE, 7)
             )
             tableview(media) {
-                readonlyColumn("Date", ImportedMedia::Created)
-                readonlyColumn("Title", ImportedMedia::Title)
-                readonlyColumn("Type", ImportedMedia::TitleType)
+                readonlyColumn("Date", Media::watchedAt)
+                readonlyColumn("Title", Media::title)
+                readonlyColumn("Type", Media::mediaType)
                 columnResizePolicy = SmartResize.POLICY
             }
         }
---
+++ src/main/kotlin/net/emteeware/Media.kt
@@ -0,0 +1,12 @@
+package net.emteeware
+
+import java.net.URL
+import java.time.LocalDate
+
+data class Media (val position: Int,
+                  val imdbId: String,
+                  val watchedAt: LocalDate,
+                  val title: String,
+                  val imdbUrl: URL,
+                  val mediaType: TraktMediaType,
+                  val YourRating: Int)
