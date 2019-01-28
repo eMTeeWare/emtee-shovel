4776176d414970b48b3b3242295922912e967c6a
Merlin Thomas <emteeware@gmail.com>
2019-01-28 23:31:32 +0100

    Remove sample data

--- src/main/kotlin/net/emteeware/MainView.kt
+++ src/main/kotlin/net/emteeware/MainView.kt
@@ -11,11 +11,7 @@ import java.time.LocalDate
 
 class MainView : View("My View") {
 
-    private var media = FXCollections.observableArrayList(
-            Media(1, "kk2342", LocalDate.of(2018, 4, 21), "asdfas", URL("https://www.example.com/kk2342"), TraktMediaType.EPISODE, 8),
-            Media(8, "kk2382", LocalDate.of(2018, 2, 21), "sda5dhf", URL("https://www.example.com/kk2382"), TraktMediaType.MOVIE, 3),
-            Media(9, "kk2392", LocalDate.of(2018, 6, 21), "asd45", URL("https://www.example.com/kk2392"), TraktMediaType.EPISODE, 7)
-    )
+    private var media = FXCollections.observableArrayList<Media>()
     override val root = borderpane {
         top = menuBox()
         center = centerVBox()
