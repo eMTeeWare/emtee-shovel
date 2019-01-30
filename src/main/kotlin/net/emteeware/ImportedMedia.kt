aaeefe2ad3f48bcd90b8dbd7a7dc7d67bf6a5489
Merlin Thomas <m.thomas@steinberg.de>
2019-01-30 08:40:06 +0100

    Extract constant

--- src/main/kotlin/net/emteeware/ImportedMedia.kt
+++ src/main/kotlin/net/emteeware/ImportedMedia.kt
@@ -6,12 +6,14 @@ import java.net.URL
 import java.time.Instant
 import java.util.*
 
+const val datepattern = "dd.MM.yyyy"
+
 data class ImportedMedia(
         val Position: Int,
         val Const: String,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = datepattern)
         val Created: Date,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = datepattern)
         val Modified: Date,
         val Description: String,
         val Title: String,
@@ -26,13 +28,13 @@ data class ImportedMedia(
         @JsonProperty("Num Votes")
         val NumVotes: Int,
         @JsonProperty("Release Date")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = datepattern)
         val ReleaseDate: Date,
         val Directors: String,
         @JsonProperty("Your Rating")
         val YourRating: Int,
         @JsonProperty("Date Rated")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = datepattern)
         var DateRated: Date?) {
     init {
         try {
