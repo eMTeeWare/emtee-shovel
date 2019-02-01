74f07dc30451277e7e43005aaef0f8137b598611
Merlin Thomas <emteeware@gmail.com>
2019-02-01 18:55:45 +0100

    Remove command line main logic
    Project is now a full-fledged TornadoFX UI application

--- src/main/kotlin/net/emteeware/EmteeShovel.kt
+++ src/main/kotlin/net/emteeware/EmteeShovel.kt
@@ -1,20 +1,8 @@
 package net.emteeware
 
-import java.time.LocalDate
-import java.time.Period
+import tornadofx.*
 
-
-fun main(args: Array<String>) {
-    if(args.isEmpty() || args.size > 1){
-        println("Please give the csv file exported from IMDb as the only parameter.")
-        System.exit(1)
-    }
-    val imdbViewingHistory = ImdbViewingHistory()
-    imdbViewingHistory.importFromCsv(args[0])
-    imdbViewingHistory.removeUnrated()
-    imdbViewingHistory.removeSeenAfter(LocalDate.of(2018, 4, 21))
-    imdbViewingHistory.removeUndefined() // Some of these should be handled as episodes or movies, there should be a manual process for that
-    imdbViewingHistory.removeDuplicateCheckInsWithin(Period.ofDays(14))
-    imdbViewingHistory.print()
+class EmteeShovelKt : App() {
+    override val primaryView = MainView::class
 }
 
